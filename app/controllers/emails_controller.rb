57738d5ddc0c0627902305bf50e5fc5e497331a6
Connor Barnett <connorb@stanford.edu>
2014-02-18 20:52:15 -0800

    fist stab at our loading page

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,27 @@
+class EmailsController < ApplicationController
+	def new
+		@email = Email.new
+	end
+
+	def create
+		@email = Email.new(email_params)
+		if @email.save
+			redirect_to @email
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@email = Email.find(params[:id])
+	end
+
+	def index
+		@emails = Email.all
+	end
+
+	private
+		def email_params
+			params.require(:email).permit(:email);
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LandingPage::Application.config.secret_key_base = '45c96141eb4419556519e45485a51b66da343a2e2e1fbc283a325f9953a34bcc7dd12f71454f1520ef07ec6d968f43922631e3ee8ae7c97ddda899d6fbd0affb'
